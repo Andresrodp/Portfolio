fc1d09c3644ea96abb11231c612eda026dac44c8
Andresrodp <arodriguezp.cs.1@gmail.com>
2023-02-15 10:01:22 -0500

    mejoras en el diseño cambio en cards de proyecto

--- client/src/components/Proyectos/Card.jsx
+++ client/src/components/Proyectos/Card.jsx
@@ -4,8 +4,17 @@ import styles from './Card.module.css';
 const CardHome = (props) => {
   return (
     <div className={styles.mainContanier} >
-      <h2>{props.titulo}</h2>
-      <img src={props.img} alt="img_card" />
+      <div className={styles.divImg}>
+        <img src={props.img} alt="img_card" />
+      </div>
+      <div className={styles.divInfo}>
+        <div className={styles.divHeaderInfo}>
+          <h2>{props.titulo}</h2>
+          <button>Ver más</button>
+        </div>
+        <p>Esto es hardcodeado</p>
+      </div>
+
     </div>
   )
 }
