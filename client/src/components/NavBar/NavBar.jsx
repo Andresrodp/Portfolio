d3380df8dcc6b5d8768a095103679fe99343b6d7
Andresrodp <arodriguezp.cs.1@gmail.com>
2023-02-19 20:09:49 -0500

    diseño responsive

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import styles from './NavBar.module.css'
 import fotoPerfil from '../../assets/imagenes/Andres.png'
 import { NavLink, useNavigate } from "react-router-dom";
+import {
+  AiFillHome,
+  AiFillFolder,
+  AiFillPhone
+} from 'react-icons/ai';
 
 
 const NavBar = () => {
@@ -18,9 +23,9 @@ const NavBar = () => {
           <h3>Andrés ✌</h3>
         </div>
         <div className={styles.divLinks}>
-          <NavLink className={styles.NavigationLinks} to='/home'><p>Inicio</p></NavLink>
-          <NavLink className={styles.NavigationLinks} to='/proyectos'><p>Proyectos</p></NavLink>
-          <NavLink className={styles.NavigationLinks} to='/contacto'><p>Contáctame</p></NavLink>
+          <NavLink className={styles.NavigationLinks} to='/home'><AiFillHome className={styles.icons} /><p>Inicio</p></NavLink>
+          <NavLink className={styles.NavigationLinks} to='/proyectos'><AiFillFolder className={styles.icons} /><p>Proyectos</p></NavLink>
+          <NavLink className={styles.NavigationLinks} to='/contacto'><AiFillPhone className={styles.icons} /><p>Contáctame</p></NavLink>
         </div>
       </div>
     </>
